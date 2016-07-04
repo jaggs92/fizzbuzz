41f1a62f980b0078f9e11ff9f40db7269ee099aa
Harry Jaggs <harryjaggs92@gmail.com>
2016-07-04 19:23:23 +0000

    4th rewrite

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,22 @@
-def fizzbuzz (number)
-    return 0 if number == 0
-    if number % 3 ==0 && number % 5 == 0
-        'fizzbuzz'
-        elsif number % 3 == 0
-        'fizz'
-        elsif number % 5 == 0
-        'buzz'
-    else number
-    end
+def fizzbuzz(number)
+    return 'FizzBuzz'if is_divisible_by_fifteen?(number)
+    return 'Buzz' if is_divisible_by_five?(number)
+    return 'Fizz' if is_divisible_by_three?(number)
+    number
 end
+  
+def is_divisible_by_three?(number)
+  is_divisible_by?(number, 3)
+end
+
+def is_divisible_by_five?(number)
+  is_divisible_by?(number, 5)
+end
+
+def is_divisible_by_fifteen?(number)
+  is_divisible_by?(number, 15)
+end
+
+def is_divisible_by?(number, divisor)
+  number % divisor == 0
+  end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,20 +1,40 @@
 require 'fizzbuzz'
 
-describe 'fizzbuzz' do
-    it 'returns "fizz" when passed 3' do
-        expect (fizzbuzz(3)).to eq 'fizz'
+describe 'FizzBuzz' do
+  context 'knows that a number is divisible by' do
+      it '3' do
+          expect (is_divisible_by_three?(3)).to be_true
     end
-    it 'returns "buzz" when passed 5' do
-        expect (fizzbuzz(5)).to eq 'buzz'
+    it '5' do
+            expect (is_divisible_by_five?(5)).to be_true
     end
-    it 'returns "fizzbuzz" when passed 15' do
-        expect (fizzbuzz(15)).to eq 'fizzbuzz'
-    end
-
-    it 'returns num when not divisible by 3 or 5' do
-        expect(fizzbuzz(7)).to eq 7
+    it '15' do
+            expect (is_divisible_by_fifteen?(15)).to be_true
     end
 end
-
-
-
+  context 'know that a number is not divisible by' do
+    it '3' do
+            expect (is_divisible_by_three?(1)).not_to be_true
+        end
+    it '5' do
+        expect (is_divisible_by_five?(1)).not_to be_true
+        end
+    it '15' do
+        expect (is_divisible_by_fifteen?(1)).not_to be_true
+        end
+  end
+  context 'while playing the game it returns' do
+        it 'the number' do
+            expect(fizzbuzz(1)).to eq 1
+        end
+        it 'Fizz'do
+            expect(fizzbuzz(3)).to eq 'Fizz'
+        end
+        it 'Buzz' do
+            expect(fizzbuzz(5)).to eq 'Buzz'
+        end
+        it 'FizzBuzz' do
+            expect(fizzbuzz(15)).to eq 'FizzBuzz'
+        end
+  end
+end
