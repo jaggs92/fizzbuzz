570e595fbbdb603dc326954eca5a785b9bcde854
Harry Jaggs <harryjaggs92@gmail.com>
2016-07-05 08:20:15 +0000

    6th rewrite

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,12 +1,5 @@
-def fizzbuzz(number)
-  return 'FizzBuzz' if is_divisible_by_fifteen?(number)
-  return 'Buzz' if is_divisible_by_five?(number)
-  return 'Fizz' if is_divisible_by_three?(number)
-  number
-end
-
 def is_divisible_by_three?(number)
-  is_divisible_by?(number, 3)
+ is_divisible_by?(number, 3)
 end
 
 def is_divisible_by_five?(number)
